a89f23c6c1c5d2aa6ca954d6a5c3e6826a9ad4f8
Elias Kalyvas <iliaskalivas@hotmail.com>
2026-05-27 17:23:37 +0300

    Fix service worker registration

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
@@ -16,12 +11,6 @@ if ('serviceWorker' in navigator) {
       .catch((err) => console.error('SW error', err))
   })
 }
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
