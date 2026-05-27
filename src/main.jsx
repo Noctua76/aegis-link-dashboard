d8cea1c558fc2fd9ecb2f52562640d4191047985
Elias Kalyvas <iliaskalivas@hotmail.com>
2026-05-27 17:19:43 +0300

    PWA: register service worker

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(() => console.log('Aegis PWA ready'))
+      .catch((err) => console.error('SW error', err))
+  })
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
