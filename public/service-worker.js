01278df4b875797769b7fc9ab5918a7375a00c12
Elias Kalyvas <iliaskalivas@hotmail.com>
2026-05-27 20:30:55 +0300

    Simplify PWA manifest for Samsung Browser

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "aegis-dashboard-v3";
+const CACHE_NAME = "aegis-dashboard-v4";
 
 const STATIC_ASSETS = [
   "/aegis-link-dashboard/",
@@ -11,8 +11,11 @@ const STATIC_ASSETS = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(STATIC_ASSETS))
+    caches.open(CACHE_NAME).then(async (cache) => {
+      await Promise.allSettled(
+        STATIC_ASSETS.map((asset) => cache.add(asset))
+      );
+    })
   );
 
   self.skipWaiting();
@@ -36,12 +39,6 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
